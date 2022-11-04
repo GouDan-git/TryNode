2a2f5cf321ab781a6242a0dff285deacaa1e5051
GouDan <1365815077@qq.com>
2022-11-04 15:01:16 +0800

    express服务（封装了node自带http服务）

---
+++ express服务.js
@@ -0,0 +1,30 @@
+const express = require("express");
+// 创建web服务器
+const app = express();
+
+// 对外提供静态资源（"/public"为路径前缀）
+app.use(express.static("./public"));
+app.use("/public", express.static("./public"));
+
+// 监听get请求
+app.get("/user", (req, res) => {
+  res.send({ name: "你的名字" });
+});
+app.get("/", (req, res) => {
+  // req.query 可以接收url末尾的参数
+  res.send(req.query);
+});
+app.get("/page/:size/:number", (req, res) => {
+  // req.params 可以接收动态参数
+  res.send(req.params);
+});
+
+// 监听post请求
+app.post("/user", (req, res) => {
+  res.send("post请求成功");
+});
+
+// 启动web服务器
+app.listen("80", () => {
+  console.log("express running at http://127.0.0.1");
+});
--- node服务.js
+++ node服务.js
@@ -1,15 +1,19 @@
-const http = require('http')
-const fs = require('fs')
-const path = require('path')
-const server = http.createServer()
-server.on('request', (req, res) => {
-	const fpath = path.join(__dirname, req.url)
-	console.log('get path', fpath)
-	fs.readFile(fpath, 'utf8', (err, dataStr) => {
-		if (err) return res.end('<h1>404 Not Fount.</h1>')
-		res.end(dataStr)
-	})
-})
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const server = http.createServer();
+// 监听request请求（get，post等）
+server.on("request", (req, res) => {
+  // 获取请求路径req.url，与本地路径__dirname拼接
+  const fpath = path.join(__dirname, req.url);
+  console.log("get path", fpath);
+  // 读取本地文件
+  fs.readFile(fpath, "utf8", (err, dataStr) => {
+    if (err) return res.end("<h1>404 Not Fount.</h1>");
+    res.end(dataStr);
+  });
+});
+// 启动http服务器
 server.listen(80, () => {
-	console.log('server running at http://127.0.0.1')
-})
+  console.log("server running at http://127.0.0.1");
+});
